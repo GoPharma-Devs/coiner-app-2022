bb56c41a52f0678e611c5f65174ed464a7d5947f
crbzxy <carlos.boor@gmail.com>
2022-09-19 12:05:45 -0500

    fix links :wq

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -10,7 +10,7 @@ const Banner = () => (
       title="No te pierdas el congreso "
       subtitle="24, 25, 26 y 27 de Noviembre 2022"
       button={
-        <Link href="/contacto">
+        <Link href="/registro">
           <a>
             <Button>Registro</Button>
           </a>
