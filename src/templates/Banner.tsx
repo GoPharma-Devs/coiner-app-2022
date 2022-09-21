bbd4ed134ded7f880afb6c22675d258d7d2ebbc8
crbzxy <carlos.boor@gmail.com>
2022-09-20 19:19:43 -0500

    add

--- src/templates/Banner.tsx
+++ src/templates/Banner.tsx
@@ -8,7 +8,7 @@ const Banner = () => (
   <Section>
     <CTABanner
       title="No te pierdas el congreso "
-      subtitle="24, 25, 26 y 27 de Noviembre 2022"
+      subtitle="24, 25, 26 y 27 de noviembre 2022."
       button={
         <Link href="/registro">
           <a>
