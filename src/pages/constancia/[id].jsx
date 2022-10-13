1677a55636178f02874c2c4ce7d77cd407427673
crbzxy <carlos.boor@gmail.com>
2022-10-12 20:58:17 -0500

    update id for constancia

---
+++ src/pages/constancia/[id].jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Head from 'next/head';
+
+function ConstanciaView({ contancia }) {
+  return (
+    <>
+      <Head>
+        <title>Congreso Coiner® | Constancias</title>
+        <meta name="description" content="Constancia Congreso Coiner" />
+      </Head>
+
+      <section className="h-screen flex align-middle content-center p-4">
+        <h1>{contancia.name}</h1>
+      </section>
+    </>
+  );
+}
+
+export default ConstanciaView;
+
+export const getServerSideProps = async (ctx) => {
+  const res = await fetch(`http://localhost:3000/api/users/${ctx.query.id}`);
+  const contancia = await res.json();
+
+  return {
+    props: { contancia },
+  };
+};
